26183b42eb0961e06f150a7ba37011f9bf38f81d
zhoujiafei <564751169@qq.com>
2016-04-18 23:12:07 +0800

    调整控制器

--- backend/controllers/StatisticsController.php
+++ backend/controllers/StatisticsController.php
@@ -16,12 +16,12 @@ class StatisticsController extends BaseBackController
     //统计页面
     public function actionIndex() {
         //获取订单总数
-        $pay_type = intval(Yii::$app->request->get('pay_type',0));//付款方式
-        $pay_status = intval(Yii::$app->request->get('pay_status',0));//付款付款状态
-        $status = intval(Yii::$app->request->get('status',0));//订单状态
-        $store_id = intval(Yii::$app->request->get('store_id',0));//商家ID
-        $stime = strtotime(Yii::$app->request->get('stime',''));//开始时间
-        $etime = strtotime(Yii::$app->request->get('etime',''));//结束时间
+        $pay_type = intval(Yii::$app->request->post('pay_type',0));//付款方式
+        $pay_status = intval(Yii::$app->request->post('pay_status',0));//付款付款状态
+        $status = intval(Yii::$app->request->post('status',0));//订单状态
+        $store_id = intval(Yii::$app->request->post('store_id',0));//商家ID
+        $stime = strtotime(Yii::$app->request->post('stime',''));//开始时间
+        $etime = strtotime(Yii::$app->request->post('etime',''));//结束时间
         
         $condition = [];
         if (!empty($pay_type)) {
