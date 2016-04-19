6f3cfc4d3827af9043eee4908908dbc8f2da968a
zhoujiafei <564751169@qq.com>
2016-04-19 16:24:23 +0800

    去掉前台没有必要的文件

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,12 +1,10 @@
 <?php
 $params = array_merge(
     require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
     require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
 );
 
-return [
+$config = [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
@@ -29,6 +27,10 @@ return [
                 ],
             ],
         ],
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'HJPMC1BaIJqdAFzkayd8hPhqynmI5cpl',
+        ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
@@ -41,3 +43,17 @@ return [
     ],
     'params' => $params,
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
